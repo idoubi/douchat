46e249e19f636bd1dd742e258103a56845ae3bac
艾逗笔 <mikemintang@126.com>
2016-09-01 10:18:09 +0800

    解决编辑器路径问题

--- App/Mp/Behavior/EditorBehavior.class.php
+++ App/Mp/Behavior/EditorBehavior.class.php
@@ -14,7 +14,7 @@ class EditorBehavior extends Behavior {
 		switch ($editor_type) {
 			case 'markdown':
 				$upload_url = U('Material/markdown_picupload');
-				$html = '<script type="text/javascript" src="/Public/Plugins/editormd/js/editormd.min.js"></script>
+				$html = '<script type="text/javascript" src="'.SITE_URL.'Public/Plugins/editormd/js/editormd.min.js"></script>
                         <script type="text/javascript">
                         var testEditor;
                         $(function() {
@@ -23,14 +23,14 @@ class EditorBehavior extends Behavior {
                                 height  : 640,
                                 syncScrolling : "single",
                                 emoji : true,
-                                path    : "/Public/Plugins/editormd/lib/",
+                                path    : "'.SITE_URL.'Public/Plugins/editormd/lib/",
                                 imageUpload : true,
                                 imageFormats : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
                                 imageUploadURL : "'.$upload_url.'",
                             });
                         });
                         </script>
-                        <link rel="stylesheet" href="/Public/Plugins/editormd/css/editormd.css" />
+                        <link rel="stylesheet" href="'.SITE_URL.'Public/Plugins/editormd/css/editormd.css" />
                         <div class="form-group">
                             <label class="col-sm-2 col-xs-3 control-label">'.$params['title'].':</label>
                             <div class="col-sm-9 col-xs-12">
