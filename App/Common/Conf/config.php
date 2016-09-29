2b067120805c509b1072f0267bee34491eb4cc26
斯文 <386179555@qq.com>
2016-09-29 13:15:56 +0800

    第一次安装，修改后台首页git地址

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -1,6 +1,14 @@
 <?php
 
 return array(
+	
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => '127.0.0.1', // 服务器地址
+    'DB_NAME'   => 'douchat', // 数据库名
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => 'psw960516',  // 密码
+    'DB_PORT'   => '3306', // 端口
+    'DB_PREFIX' => 'dc_', // 数据库表前缀
 
     'HTTP_HOST' => $_SERVER['HTTP_HOST'],   // 当前域名
 
