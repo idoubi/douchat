3240f348292f96c09b66afbf32911d4786e24288
艾逗笔 <mikemintang@126.com>
2016-12-22 21:20:06 +0800

    修改导入css和js的行为类，采用cdn加速的方式

--- App/Mp/Behavior/ImportJsBehavior.class.php
+++ App/Mp/Behavior/ImportJsBehavior.class.php
@@ -14,16 +14,32 @@ class ImportJsBehavior extends Behavior {
 			foreach ($params as $k => $v) {
 				switch ($v) {
 					case 'jquery':
-						$res[] = '<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.js"></script>';
+						if (strpos($k, '.') === false) {
+							$res[] = '<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>';
+						} else {
+							$res[] = '<script src="//cdn.bootcss.com/jquery/'.$k.'/jquery.min.js"></script>';
+						}
 						break;
-					case 'jquery.min':
-						$res[] = '<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>';
-						break;
-					case 'zepto.min':
-						$res[] = '<script src="//cdn.bootcss.com/FrozenUI/1.3.0/lib/zepto.min.js"></script>';
+					case 'zepto':
+						if (strpos($k, '.') === false) {
+							$res[] = '<script src="//cdn.bootcss.com/zepto/1.0rc1/zepto.min.js"></script>';
+						} else {
+							$res[] = '<script src="//cdn.bootcss.com/zepto/'.$k.'/zepto.min.js"></script>';
+						}
 						break;
 					case 'frozen':
-						$res[] = '<script src="//cdn.bootcss.com/FrozenUI/1.3.0/js/frozen.js"></script>';
+						if (strpos($k, '.') === false) {
+							$res[] = '<script src="//cdn.bootcss.com/FrozenUI/1.3.0/js/frozen.js"></script>';
+						} else {
+							$res[] = '<script src="//cdn.bootcss.com/FrozenUI/'.$k.'/js/frozen.js"></script>';
+						}
+						break;
+					case 'vue':
+						if (strpos($k, '.') === false) {
+							$res[] = '<script src="//cdn.bootcss.com/vue/2.1.3/vue.min.js"></script>';
+						} else {
+							$res[] = '<script src="//cdn.bootcss.com/vue/'.$k.'/vue.min.js"></script>';
+						}
 						break;
 					default:
 						# code...
@@ -33,16 +49,16 @@ class ImportJsBehavior extends Behavior {
 		} else {
 			switch ($params) {
 				case 'jquery':
-					$res[] = '<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.js"></script>';
-					break;
-				case 'jquery.min':
 					$res[] = '<script src="//cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>';
 					break;
-				case 'zepto.min':
-					$res[] = '<script src="//cdn.bootcss.com/FrozenUI/1.3.0/lib/zepto.min.js"></script>';
+				case 'zepto':
+					$res[] = '<script src="//cdn.bootcss.com/zepto/1.0rc1/zepto.min.js"></script>';
 					break;
 				case 'frozen':
 					$res[] = '<script src="//cdn.bootcss.com/FrozenUI/1.3.0/js/frozen.js"></script>';
+					break;
+				case 'vue':
+					$res[] = '<script src="//cdn.bootcss.com/vue/2.1.3/vue.min.js"></script>';
 					break;
 				default:
 					# code...
