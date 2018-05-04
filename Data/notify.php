2b2e91042c8923ac822e4456e720a12a9f2a1b8d
mikemintang <mikemintang@126.com>
2018-05-04 16:02:41 +0800

    修复高版本php接收不到支付异步通知数据的问题

--- Data/notify.php
+++ Data/notify.php
@@ -4,8 +4,9 @@
  * 微信支付异步通知处理程序
  * @author 艾逗笔<765532665@qq.com>
  */
-$xml = $GLOBALS["HTTP_RAW_POST_DATA"];		// 获取微信支付异步通知数据
-file_put_contents('test.txt', $xml);
+ini_set('always_populate_raw_post_data',-1);
+$xml = file_get_contents('php://input');
+
 $arr = json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);		// 将xml格式的数据转换为array数组
 
 $attach = $arr['attach'];											// 获取通知中包含的附加参数
